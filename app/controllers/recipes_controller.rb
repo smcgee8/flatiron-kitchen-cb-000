a05abd1069f93cb25d3cae01f1c0493cc41024e2
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-10 17:29:03 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,16 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
 end
